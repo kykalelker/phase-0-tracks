6eb14f0f65665499603be9da083160b658877924
Kanan Kalelker <kykalelker@gmail.com>
2017-04-21 16:53:00 -0700

    check to see if newhire is a vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,9 +2,6 @@ puts "What is your name?"
 newhire_name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
-if age<=104
-  age = true
-else age = false
 puts "In what year were you born?"
 birth_year = gets.chomp.to_i
 puts "Our cafeteria makes the best garlic bread, You should try it with your coffee. Should I order up some for you? (y/n)"
@@ -15,6 +12,7 @@ if wants_garlicbread == "y"
  wants_garlicbread = false
  else puts "Sorry, I don't understand"
  end
+ 
 puts "We have a very comprehensive health insurance policy. Would you like to us to enroll you in it? (y/n)"
 wants_insurance = gets.chomp
  if wants_insurance == "y"
@@ -24,6 +22,13 @@ wants_insurance = gets.chomp
  else puts "Sorry, I don't understand"
  end
 
-if age && (wants_garlicbread || wants_insurance)
+if (newhire_name == "DrakeCula") || (newhire_name == "TuFang")
+  puts "Definitely a vampire."
+elsif (age<=100) && (wants_garlicbread || wants_insurance)
   puts "Probably not a vampire."
-elsif !age && ()
+elsif !(age<=100) && !(wants_garlicbread && wants_insurance)
+  puts "Probably a vampire."
+elsif !(age<=100) && !(wants_garlicbread || wants_insurance)
+  puts "Almost certainly a vampire."
+else puts "Results Inconclusive."   
+end 
