b652a0cf7aa252839eb3aff3ce089150a004a461
Kanan Kalelker <kykalelker@gmail.com>
2017-04-21 11:45:27 -0700

    Halt just before release2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,9 +2,28 @@ puts "What is your name?"
 newhire_name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
+if age<=104
+  age = true
+else age = false
 puts "In what year were you born?"
 birth_year = gets.chomp.to_i
-puts "Our cafeteria makes the best garlic bread, You should try it with your coffee. Should I order up some for you?"
+puts "Our cafeteria makes the best garlic bread, You should try it with your coffee. Should I order up some for you? (y/n)"
 wants_garlicbread = gets.chomp
-puts "We have a very comprehensive health insurance policy. Would you like to us to enroll you in it?"
+if wants_garlicbread == "y"
+  wants_garlicbread = true
+ elsif wants_garlicbread == "n"
+ wants_garlicbread = false
+ else puts "Sorry, I don't understand"
+ end
+puts "We have a very comprehensive health insurance policy. Would you like to us to enroll you in it? (y/n)"
 wants_insurance = gets.chomp
+ if wants_insurance == "y"
+  wants_insurance = true
+ elsif wants_insurance == "n"
+ wants_insurance = false
+ else puts "Sorry, I don't understand"
+ end
+
+if age && (wants_garlicbread || wants_insurance)
+  puts "Probably not a vampire."
+elsif !age && ()
