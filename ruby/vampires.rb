fa43ae68db1f8e02083a3655fd2f4c36b2996790
Kanan Kalelker <kykalelker@gmail.com>
2017-04-21 11:15:03 -0700

    Collect initial data about new hire

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+newhire_name = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In what year were you born?"
+birth_year = gets.chomp.to_i
+puts "Our cafeteria makes the best garlic bread, You should try it with your coffee. Should I order up some for you?"
+wants_garlicbread = gets.chomp
+puts "We have a very comprehensive health insurance policy. Would you like to us to enroll you in it?"
+wants_insurance = gets.chomp
