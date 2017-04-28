40eb139ace88ea2012d4b7f2aa7e7bc2938bc3a8
Kanan Kalelker <kykalelker@gmail.com>
2017-04-28 15:31:35 -0700

    Exercise 5.6 release 2 - bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -39,3 +39,40 @@ if fib_array[-1] == fib_100
 else
   puts "back to work!"
 end
+
+
+# Release 2
+=begin 
+  Bubble Sort Algorithm
+  -Create an array of 5 unsorted integers
+  - Sort in ascending order
+  1. Create 2 indexes a & b to compare values from 2 cells at a time
+  2. Start comparison at a indexed to 0 and b indexed to 1 (next cell)
+  3. if value at index b is lower than value at a: swap
+  else leave and go to next cell (move index b to the next cell)
+  4. Repeat until all cells are done for a = 0
+  5. Repeat from a = 0 to a = last position in the array
+=end
+
+
+def sortarray (sort_me)
+  counter = sort_me.length
+  a = 0
+  b = 1
+  while a < (counter-1)
+    b = a+1
+    while b < (counter)
+      if sort_me[a] > sort_me[b]
+        temp = sort_me[a]
+        sort_me[a] = sort_me[b]
+        sort_me[b] = temp
+      end
+      b += 1
+    end
+    a += 1
+  end
+  return sort_me
+end
+sort_me = [7, 3, 1, 9, 5]
+p sort_me
+sortarray (sort_me)
