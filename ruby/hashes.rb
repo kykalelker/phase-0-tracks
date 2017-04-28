25e2e3cdccc1470441255d2bfdbb83d5aae143b6
Kanan Kalelker <kykalelker@gmail.com>
2017-04-28 15:59:11 -0700

    Define hash to cqpture user data

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+#Create a form to capture client details for interior design purposes
+
+#Define client data hash with essential keys
+client_data = {
+  name: "",
+  address: "",
+  email: "",
+  phone: "",
+  gender_female: "",
+  children: "",
+  decor_theme: "",
+  budget_cutoff: ""
+}
+
