ce3c49fcc28edf02e5dc75e134e4dafb9bc46ac6
Kanan Kalelker <kykalelker@gmail.com>
2017-04-28 16:00:25 -0700

    Update hash key depending on user input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -34,3 +34,20 @@ client_data[:decor_theme] = gets.chomp
 puts "And finally, what is your budget"
 client_data[:budget_cutoff] = gets.chomp.to_i
 
+#Print hash back to the screen
+puts "Kindly confirm your details"
+p client_data
+#Ask if anything needs to be updated
+puts "Would you like to update anything?"
+if !(gets.chomp == "none")
+#Get field to convert from user 
+puts "What would you like to update?"
+update_key = gets.chomp
+puts " What is your #{update_key}? "
+#Convert input field to symbol and use it to store value
+client_data[update_key.to_sym] = gets.chomp
+end  
+
+#Print updated hash to the screen
+puts "Here are all your most updated details"
+p client_data
