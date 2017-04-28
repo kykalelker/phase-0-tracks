64928d278264d626ace80e1f7f4b752890bc66c4
Kanan Kalelker <kykalelker@gmail.com>
2017-04-28 15:59:46 -0700

    Ask user for details

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -12,3 +12,25 @@ client_data = {
   budget_cutoff: ""
 }
 
+#Ask user for data
+puts "In order to do a good job of decorating your home, we need some details from you:"
+puts "What is your name?"
+client_data[:name] = gets.chomp
+puts "What is your address?"
+client_data[:address] = gets.chomp
+puts "What is your email?"
+client_data[:email] = gets.chomp
+puts "What is your phone number?"
+client_data[:phone] = gets.chomp.to_i
+puts "Do you identify with being female?"
+if gets.chomp == "yes" 
+  client_data[:gender_female] = true
+else client_data[:gender_female] = false
+end
+puts "How many children do you have?"
+client_data[:children] = gets.chomp.to_i
+puts "What decor theme do you like?"
+client_data[:decor_theme] = gets.chomp
+puts "And finally, what is your budget"
+client_data[:budget_cutoff] = gets.chomp.to_i
+
