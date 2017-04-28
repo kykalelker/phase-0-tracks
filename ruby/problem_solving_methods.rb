802d5d0c0ce0bdb49d61e883e92f8c389237a501
Kanan Kalelker <kykalelker@gmail.com>
2017-04-28 15:19:27 -0700

    Exercise 5.6 release 1 - fibonacci series

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -15,3 +15,27 @@ x = 23
 p search_array(arr, x)
 x = 45
 p search_array(arr, x)
+
+
+# Release 1
+# Fibonacci
+def calc_fibonacci (x)
+  fib = [0, 1] 
+  counter = 2
+  while counter < x 
+    data_point = fib[-1] + fib[-2]
+    fib.push data_point
+    counter += 1
+  end
+  return fib
+end
+puts "how many terms would you like to see?"
+x = gets.chomp.to_i
+fib_array = calc_fibonacci(x)
+p fib_array 
+fib_100 = 218922995834555169026
+if fib_array[-1] == fib_100
+  puts "Yay"
+else
+  puts "back to work!"
+end
