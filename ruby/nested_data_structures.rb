3a66c5b250946e665952f57e25398abd77ca2fe3
Kanan Kalelker <kykalelker@gmail.com>
2017-04-29 16:13:02 -0700

    Create nested data structure with hashes and arrays

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,71 @@
+actors = {
+  american: {
+    bryan_cranston: {
+      age: 61,
+      top_performances: {
+        trumbo: "Dalton Trumbo",
+        breaking_bad: "Walt White",
+        malcolm_in_the_middle: "Hal"
+      },
+      memorable_characters: ["Heisenberg", "Tim Whatley", "Bumbling Dad"]
+    },
+    peter_dinklage: {
+      age: 47,
+      top_performances: {
+        the_station_agent: "Finbar McBride", 
+        game_of_thrones: "Tyrion Lannister"
+        },
+        memorable_characters: "The Imp"
+    }
+  },
+  british: {
+    hugh_laurie: {
+      age: 57,
+      top_performances: {
+        house_md: "Gregory House", 
+        chance: "Eldon Chance",
+        veep: "Tom James"
+        },
+        memorable_characters: "House"
+    },
+    david_tennant: {
+      age: 46,
+      top_performances: {
+        the_escape_artist: "Will Burton", 
+        jessica_jones: "Kilgrave", 
+        broadchurch: "Alec Hardy"
+      },
+      memorable_characters: ["Barty Crouch Jr", "Doctor Who"]
+    },
+    christian_bale: {
+      age: 43,
+      top_performances: {
+        the_dark_knight: "Bruce Wayne",
+        american_hustle: "Irving Rosenfeld",
+        the_machinist: "Trevor Reznik",
+        the_big_short: "Michael Burry" 
+        },
+      memorable_characters: ["Batman", "American Psycho"]
+    }
+  },
+  australian: {
+    hugh_jackman: {
+      age: 48,
+      top_performances: {
+        the_prestige: "Robert Angier", 
+        prisoners: "Keller Dover",
+        les_miserables: "Jean Valjean"
+        },
+      memorable_characters: ["Wolverine", "Leopold"]
+    },
+    chris_hemsworth: {
+      age: 33,
+      top_performances: {
+        rush: "James Hunt",
+        in_the_heart_of_the_sea: "Owen Chase"
+        },
+        memorable_characters: ["Thor", "The Huntsman"]
+    }
+  } 
+}
+p actors
