7d5eb44301cd2151beda7274a14b07dad6c0eece
Kanan Kalelker <kykalelker@gmail.com>
2017-04-29 09:00:01 -0700

    Complete code for assignment 5.3 release 0

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,31 +1,68 @@
 
 
-#def print_name
-#name1 = "Anna"
-#name2 = "Kanan"
-#puts "Hello teammates"
-#yield [name1,name2]
-#end
-#print_name {|name1,name2| puts "Good teamwork, #{name1} and #{name2}!"}
-
-student_name = ["Jim", "Jack", "Joe"]
-p student_name
-student_name.map! do |name1|
-  puts name1
-  name1.length
+# Release 0
+def print_name
+name1 = "Anna"
+name2 = "Kanan"
+puts "Hello teammates"
+yield [name1,name2]
 end
-p student_name
+print_name {|name1,name2| puts "Good teamwork, #{name1} and #{name2}!"}
 
+=begin
+# Release 1
+#Create an array and a hash
+student_name = ["Jim", "Jack", "Joe"]
 
 student_marks = {
   Jim: 89, 
   Jack: 73,
   Joe: 79
 }
+
+#.EACH 
+puts "Student names are"
+p student_name
+# Use .each on array
+student_name.each {|student| puts student.swapcase}
+#Print modified array
+p student_name
+
+puts "Student marks are:"
 p student_marks
-student_marks.each do|name1,marks|
-puts name1, marks
-end
+#Use .each on hash
+student_marks.each {|student, marks| puts "#{student} has scored #{marks} marks in his test"}
+
+
+#.MAP
+puts "Student list:"
+p student_name
+#Initialize new array
+student_caps =[]
+#Use .map on array
+stand_caps = student_name.map {|student| student.upcase}
+#Print new array to screen
+p student_caps
+p student_name
+
+puts "Student marks list:"
+p student_marks
+#Initialize new hash
+stud_performance = {}
+#Use .map on hash
+stud_performance = student_marks.map {|student, marks| [student, marks.upcase]}
+#Print new hash to screen
+p stud_performance
+
+#.MAP!
+puts "Student names are"
+p student_name
+#Use .map! on array
+student_name.map! {|name1| name1.upcase}
+#Print modified array to screen
+p student_name
+
+
 
 
  # Release 2
@@ -55,3 +92,4 @@ student_marks = {
 student_marks.flatten {|name, marks|}
 
 # 2. (Hash)
+=end
