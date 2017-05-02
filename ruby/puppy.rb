df53605ffc30b36554ca814c4ccd6ad0d055189c
Kanan Kalelker <kykalelker@gmail.com>
2017-05-02 12:28:21 -0700

    Create class of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+=begin
+SPECIES
+puppius kyutus
+
+CHARACTERISTICS
+Size: Varies
+Color: varies
+Age: 1 year or under
+Name: varies
+Nature: friendly
+Likes biscuits: yes
+Puppy dog eyes: yes
+
+BEHAVIOR
+Bark
+Retrieve
+Sit
+Chase
+Play
+
+=end
