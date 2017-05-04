86b1368d735a46fef45c4cfd9bdf4cc57a4cc921
Kanan Kalelker <kykalelker@gmail.com>
2017-05-04 09:48:30 -0700

    Create shout module as a mix in

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,3 +11,30 @@ end
 
 puts Shout.yell_angrily("what the heck")
 puts Shout.yelling_happily("nicely done")
+=end
+
+module Shout
+  def yell_angrily(words)
+    puts "Mommy says " + words + "!!!" + " :("
+  end
+  
+  def yelling_happily(exclamation)
+    puts "Child says " + "Yayy!! " + exclamation + " Yayy!!" + " :-p"
+  end
+end
+
+class Mommy
+  include Shout 
+end
+
+class Child
+  include Shout
+end
+
+mommy = Mommy.new 
+mommy.yell_angrily("Don't do that")
+
+kiddo = Child.new
+kiddo.yelling_happily("ice cream")
+
+
