1e721b5cab37bc2735118d910d63ed8f3112f0d6
Kanan Kalelker <kykalelker@gmail.com>
2017-05-10 14:16:38 -0700

    Adjust pseudocode to add detail to algorithm steps and add if else condition to print reversed string

--- js/explore.js
+++ js/explore.js
@@ -1,25 +1,32 @@
 //Pseudocode
 //1. Initialize a string variable and assign a value
 //2. write a function to print out the string in reverse.
-//  a. calculate the length of the string
-//  b. Initialize a counter and assign the string length to it.
-//  c. Count down from the string length to 1. 
-//  d. For every count, print the equivalent letter from the string.
-//  e. Store resulting string in a variable
+//  a. Declare an empty string known as rev_string
+//  b. calculate the length of the string
+//  c. Initialize a counter and assign the string length - 1 to it.
+//  d. Count down from the string length to 0. 
+//  e. For every count, add the equivalent letter from the original string to rev_string.
+//  f. After loop is complete return rev_string
 //3. print new variable if a condition is true
 
 
 
 function reverse(string) {
-  var rev_string = "";
+  var revString = "";
   for (var i = string.length-1; i >= 0; i--) {
-    rev_string += string[i];
+    revString += string[i];
   }
-  return rev_string;
+  return revString;
 }
 
 var str = "Anybody";
+var printReversed = true;
+reversedString = reverse(str);
 
-console.log(reverse(str));
-
+if (printReversed){
+console.log(reversedString);
+}
+else {
+  console.log(str);
+}
 
