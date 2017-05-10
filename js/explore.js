38b6de618784d799103e5716fe1b92a50ce7c3ae
Kanan Kalelker <kykalelker@gmail.com>
2017-05-10 10:32:31 -0700

    Write pseudocode for program

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+//Pseudocode
+//1. Initialize a string variable and assign a value
+//2. write a function to print out the string in reverse.
+//  a. calculate the length of the string
+//  b. Initialize a counter and assign the string length to it.
+//  c. Count down from the string length to 1. 
+//  d. For every count, print the equivalent letter from the string.
+//  e. Store resulting string in a variable
+//3. print new variable if a condition is true
+
