011b5256d9e6a998e61ec451786d0be136134705
Kanan Kalelker <kykalelker@gmail.com>
2017-05-22 07:28:40 -0700

    Initial commit

---
+++ databases/ruby_sql/choreslist.rb
@@ -0,0 +1,13 @@
+# require gems
+require 'SQLite3'
+
+# create SQLite3 database
+todo = SQLite3::Database.new("chores.db")
+
+# create chore table
+
+# populate responsibility and chore tables
+
+# driver code
+
+# retrieve data
