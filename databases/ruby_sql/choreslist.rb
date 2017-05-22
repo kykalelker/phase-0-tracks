3bef5c493ec48060f9fe890cf7943887a53b2dba
Kanan Kalelker <kykalelker@gmail.com>
2017-05-22 07:32:39 -0700

    Add driver code to call on methods

--- databases/ruby_sql/choreslist.rb
+++ databases/ruby_sql/choreslist.rb
@@ -35,6 +35,23 @@ def add_chores(todo, jobs_to_do, minutes, person_num)
 end
 
 # driver code
+puts "How many chores would you like to assign?"
+num_of_chores = gets.chomp.to_i
+person = []
+num_of_chores.times do
+  puts "What chore do you want completed?"
+  jobs_to_do = gets.chomp
+  puts "How many minutes does the chore take to complete?"
+  minutes = gets.chomp.to_i
+  puts "Whose responsibility is it to complete this task?"
+  temp = gets.chomp
+  if !person.include?(temp)
+    person << temp
+    add_respons(todo, temp)
+  end
+  person_num = person.index(temp) + 1
+  add_chores(todo, jobs_to_do, minutes, person_num)
+end
 
 # retrieve data
 chores = todo.execute("SELECT chores.task, chores.time_taken, responsibility.name FROM chores JOIN responsibility ON chores.respons_id = responsibility.id")
