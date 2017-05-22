1ec1bcf02187ca1779742f7453a5b38e5d162934
Kanan Kalelker <kykalelker@gmail.com>
2017-05-22 07:30:21 -0700

    Create chore and responsibility tables in chores.db

--- databases/ruby_sql/choreslist.rb
+++ databases/ruby_sql/choreslist.rb
@@ -4,7 +4,26 @@ require 'SQLite3'
 # create SQLite3 database
 todo = SQLite3::Database.new("chores.db")
 
-# create chore table
+# create chore and responsibility tables
+create_chore_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS chores(
+    id INTEGER PRIMARY KEY,
+    task VARCHAR (255),
+    time_taken INTEGER,
+    respons_id INT,
+    FOREIGN KEY (respons_id) REFERENCES responsibility(id)
+  )
+  SQL
+
+create_responsibility_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS responsibility(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR (255)
+  )
+  SQL
+
+todo.execute(create_responsibility_table)
+todo.execute(create_chore_table)
 
 # populate responsibility and chore tables
 
